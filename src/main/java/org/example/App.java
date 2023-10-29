1fd39c300c88f24bed902289cf6d917bdb18c5d8
GunnarGoteborg <149320656+GunnarGoteborg@users.noreply.github.com>
2023-10-29 18:16:38 +0100

    Update App.java
    added WWhy

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,7 @@ package org.example;
 public class App {
     public int add(int a,int b){
         System.out.println("added comment yes or no");
+        System.out.println("added comment why");
         return a+b;
     }
 }
