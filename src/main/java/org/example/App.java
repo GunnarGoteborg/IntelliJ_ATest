b9e872137b52a5123fe4fb35b2b2e26291e34417
GunnarGoteborg <149320656+GunnarGoteborg@users.noreply.github.com>
2023-10-29 19:08:43 +0100

    Update App.java
    base

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,7 @@ public class App {
     public int add(int a,int b){
         System.out.println("added comment yes or no");
         System.out.println("added comment why oh no");
+        System.out.println("base");
         return a+b;
     }
 }
