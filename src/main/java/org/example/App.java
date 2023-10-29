07d7f4f8ad3ccd9a92923a072823c22ddee50414
GunnarGoteborg <149320656+GunnarGoteborg@users.noreply.github.com>
2023-10-29 18:20:28 +0100

    Update App.java
    oh

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,7 +3,7 @@ package org.example;
 public class App {
     public int add(int a,int b){
         System.out.println("added comment yes or no");
-        System.out.println("added comment why");
+        System.out.println("added comment why oh");
         return a+b;
     }
 }
