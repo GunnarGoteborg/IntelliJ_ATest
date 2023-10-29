9387d409df52e8b6f7818c084b716695c4d11c27
GunnarGoteborg <149320656+GunnarGoteborg@users.noreply.github.com>
2023-10-29 18:12:11 +0100

    Update App.java eith no

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class App {
     public int add(int a,int b){
-        System.out.println("added comment yes");
+        System.out.println("added comment yes or no");
         return a+b;
     }
 }
