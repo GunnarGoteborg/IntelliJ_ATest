2dd3d32cdd4dfddae93c57ea90df0b3aae870206
Gunnar <gunnar.hundrastning@gmail.com>
2023-10-30 00:11:09 +0100

    Added app.add()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        App app = new App();
+        System.out.println( app.add(3, 4));
     }
 }
