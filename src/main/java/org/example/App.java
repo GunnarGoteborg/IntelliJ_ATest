e53b254b1649e35b2e34c4982355446b1d686c9b
GunnarGoteborg <149320656+GunnarGoteborg@users.noreply.github.com>
2023-10-30 14:35:06 +0100

    Update App.java xx

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,7 @@ public class App {
         System.out.println("base it ................");
         System.out.println("puh into br???" );
         System.out.println("fifth branch" );
-        System.out.println("adding a feature uu" );
+        System.out.println("adding a feature uu xx" );
         return a+b;
     }
 }
